4114b60d83088ec3f83669b50ee2692433bceb2a
Alexander Köplinger <alex.koeplinger@outlook.com>
2025-11-12 22:40:07 +0100

    Don't upgrade npm packages anymore, we do it in emscripten

--- eng/sanitize.py
+++ eng/sanitize.py
@@ -69,17 +69,6 @@ def prune():
 if upgrade:
     for path in node_paths:
         update_npm(path)
-
-os.chdir(emscripten_path)
-rewrite_package_json("package.json")
-try:
-    os.system("npm audit fix")
-except OSError as error:
-    print("npm audit fix failed")
-    print(error)
-
-prune()
-
-if not upgrade:
+else:
     for path in node_paths:
         remove_npm(path)
